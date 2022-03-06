43dec8da50806064027fb77695a39e8698e517b6
Juan Campos <jcamposcasillas@gmail.com>
2022-03-05 18:41:50 -0700

    ts

--- src/compilador/TablaSimbolos.java
+++ src/compilador/TablaSimbolos.java
@@ -1,14 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package compilador;
+
+import java.util.*;
 
 /**
  *
  * @author Juan Pablo
  */
-public class TablaSimbolos {
-    
+
+class Simbolo {
+
+    String nombre;
+    Object valor;
+
+    public Simbolo(String nombre, Object valor) {
+        this.nombre = nombre;
+        this.valor = valor;
+    }
 }
+
+public class TablaSimbolos {
+
+    HashMap t;
+
+    public TablaSimbolos() {
+        t = new HashMap();
+    }
+
+    public Simbolo insertar(String nombre) {
+        Simbolo s = new Simbolo(nombre, null);
+        t.put(nombre, s);
+        return s;
+    }
+
+    public Simbolo buscar(String nombre) {
+        return (Simbolo) (t.get(nombre));
+    }
+
+    public void imprimir() {
+        Iterator it = t.values().iterator();
+        while (it.hasNext()) {
+            Simbolo s = (Simbolo) it.next();
+            System.out.print(s.nombre + ": ");
+            if (s.valor == null) {
+                System.out.print("i: Indefinido");
+            } else if (s.valor instanceof Integer) {
+                System.out.print("e: " + s.valor.toString());
+            } else if (s.valor instanceof String) {
+                System.out.print("c: " + s.valor);
+            }
+        }
+    }
+}
+
+
+//cambios
